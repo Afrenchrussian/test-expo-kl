236bc3f34b3ceb9663b88056250d4494510e837a
Kieran Lewin <afrenchrussian@gmail.com>
2020-03-19 00:36:45 +0000

    notch fix

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Platform, StatusBar, View } from 'react-native';
 import {WebView} from 'react-native-webview'
 
 export default function App() {
+
+  const topper = (Platform.OS === "android") ? <View style={styles.topper}/> : <View/>
+
   return (
-    <WebView source={{uri: 'https://afrenchrussian.github.io/tailored-maps-v1'}}/>
+    <SafeAreaView style={styles.container}>
+      {topper}
+      <WebView source={{uri: 'https://afrenchrussian.github.io/tailored-maps-v1'}}/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  topper: {
+    height: StatusBar.currentHeight,
+    backgroundColor: 'black'
+  }
 });
